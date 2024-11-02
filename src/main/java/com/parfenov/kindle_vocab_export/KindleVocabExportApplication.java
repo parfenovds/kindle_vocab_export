91aeb3e0b82ecec2fe89f4106dad3d765ec8fd8a
Dmitrii Parfenov <parfenovds@protonmail.com>
2024-11-02 19:40:24 +0300

    init

---
+++ src/main/java/com/parfenov/kindle_vocab_export/KindleVocabExportApplication.java
@@ -0,0 +1,13 @@
+package com.parfenov.kindle_vocab_export;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindleVocabExportApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KindleVocabExportApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/parfenov/kindle_vocab_export/KindleVocabExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parfenov.kindle_vocab_export;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindleVocabExportApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
